fb0072894a8bb1a7296ed7c2f41b5528d128c2ed
alisuzun <112907534+alisuzun@users.noreply.github.com>
2022-09-07 16:48:51 +0100

    screens and components

---
+++ components/HeaderTabs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {view, Text } from "react-native"
+
+export default function HeaderTabs() {
+  return (
+    <View>
+       {/* HeaderButton */}
+       {/* HeaderButton */}
+    </View>
+  )
+}
+
+// const HeaderButton * = ({ title, onPress }) 
